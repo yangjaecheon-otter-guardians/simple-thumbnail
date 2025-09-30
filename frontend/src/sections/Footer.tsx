b42db4abcc4cc1a9ee46eeab88cb35ec23637518
anveloper <hitedin@gmail.com>
2025-09-30 17:30:40 +0900

    fix: 헤더 로고 변경 및 README 링크 업데이트
    - Header 컴포넌트의 로고를 big_logo로 변경
    - Footer 컴포넌트의 import 순서 정렬
    - README의 GitHub 저장소 URL을 소문자로 통일
    - README에 배포 사이트 링크 추가

--- frontend/src/sections/Footer.tsx
+++ frontend/src/sections/Footer.tsx
@@ -1,5 +1,5 @@
-import github from '../assets/github.svg';
 import { ReactComponent as TEXT_LOGO } from 'assets/logo_default.svg';
+import github from '../assets/github.svg';
 
 function Footer() {
   return (
@@ -7,7 +7,7 @@ function Footer() {
       <div className="w-full px-5 flex flex-col items-center tablet:w-[768px] tablet:px-10">
         <TEXT_LOGO style={{ height: '32px' }} />
         <span>© 2023 Simple Thumbnail all rights reserved.</span>
-        <a href="https://github.com/Yangjaecheon-Otter-Guardians/simple-thumbnail" target="_blank">
+        <a href="https://github.com/yangjaecheon-otter-guardians/simple-thumbnail" target="_blank">
           <div className="flex-center gap-1 font-bold">
             <img src={github} alt="github icon" width="16rem" />
             <span>simple-thumbnail</span>
